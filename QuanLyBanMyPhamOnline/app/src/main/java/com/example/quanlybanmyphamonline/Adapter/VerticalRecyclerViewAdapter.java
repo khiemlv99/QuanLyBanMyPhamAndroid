86877d0d11445c7cad236eba057664eae6ff9dcf
tuan2698 <58164530+tuan2698@users.noreply.github.com>
2020-06-06 12:33:35 +0700

    version 1.2

---
+++ QuanLyBanMyPhamOnline/app/src/main/java/com/example/quanlybanmyphamonline/Adapter/VerticalRecyclerViewAdapter.java
@@ -0,0 +1,4 @@
+package com.example.quanlybanmyphamonline.Adapter;
+
+public class VerticalRecyclerViewAdapter {
+}
---
+++ QuanLyBanMyPhamOnline/app/src/main/java/com/example/quanlybanmyphamonline/Class/HorizontalModel.java
@@ -0,0 +1,4 @@
+package com.example.quanlybanmyphamonline.Class;
+
+public class HorizontalModel {
+}
